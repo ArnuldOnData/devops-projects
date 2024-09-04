63ea79ee1e84779af23d111226ebcfcd0bde40ff
arnuld-acog <arnuld@aganitha.ai>
2024-09-04 14:05:31 +0530

    Just structures the files

--- project-1/README.md
+++ project-1/README.md
@@ -1,1 +1,7 @@
-Sample Python Project
+# README
+
+## Sample Python project to learn GitHub Actions
+
+This program convers convers a list of temperatures from Celsium to Fahrenheit. Wanted to learn what kind of GitHub Action template I need to write to run this program and save the ouput to a text file in the current directory
+
+
---
+++ project-1/celsium-to-fahrenheit.py
@@ -0,0 +1,12 @@
+import random
+
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+# Generate a list of 10 random integers representing temperatures in Celsius
+tempslist = [random.randint(-5, 35) for _ in range(5)]
+
+# Convert each Celsius temperature to Fahrenheit and print
+for item in tempslist:
+    fahrenheit = celsius_to_fahrenheit(item)
+    print(f"{item} Celsius = {fahrenheit:.2f} Fahrenheit")
